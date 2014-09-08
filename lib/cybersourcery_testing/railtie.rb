c9938d67cd5127328a3811f34c9051f76abd9790
Michael Toppa <public@toppa.com>
2014-09-08 20:03:42 +0900

    push the gem's rake tasks into the parent Rails project

--- lib/cybersourcery_testing/railtie.rb
+++ lib/cybersourcery_testing/railtie.rb
@@ -1,10 +1,5 @@
-require 'cybersourcery_testing'
-require 'rails'
-
 module CybersourceryTesting
   class Railtie < Rails::Railtie
-    railtie_name :cybersourcery_testing
-
     rake_tasks do
       Dir[File.join(File.dirname(__FILE__),'/tasks/*.rake')].each { |f| load f }
     end
